00d4d48d5206d0c5a0a53a285d21cbafba47fc8b
veh <victor@heijler.se>
2018-02-08 22:57:26 +0100

    add tsconfig and make typescript work

--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,12 +1,16 @@
 /**
  * Main Class
  */
+
 var Main = {
+
     canvas: null,
     video: null,
     videoFeed: null,
     constraints: null,
-    init: function () {
+
+    init: function() {
+        
     }
 };
 window.addEventListener("load", Main.init, false);
--- src/ts/Webcamera.ts
+++ src/ts/Webcamera.ts
@@ -1,14 +1,16 @@
 /**
  * Represents the webcamera stream
  */
-var Webcamera = function (constraints) {
+
+var Webcamera = function(constraints) {
     if (navigator.mediaDevices) {
         navigator.mediaDevices.getUserMedia(constraints)
-            .then(function (mediaStream) {
+        .then(function(mediaStream) {
             return mediaStream;
         })
-            .catch(function (err) {
+        .catch(function(err) {
             console.log(err.name, err.message);
         });
     }
 };
+
